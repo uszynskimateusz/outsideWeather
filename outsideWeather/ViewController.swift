835712ce80033e14d0d954f1baefaf8619dba07f
uszynskimateusz <uszynskimateusz@gmail.com>
2020-09-02 16:59:32 +0200

    Initial Commit

---
+++ outsideWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  outsideWeather
+//
+//  Created by Mateusz Uszyński on 02/09/2020.
+//  Copyright © 2020 Mateusz Uszyński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
