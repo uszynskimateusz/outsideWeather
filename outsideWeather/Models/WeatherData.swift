ac09f754c3e43ab7384a364e8a69dec5deb52602
uszynskimateusz <uszynskimateusz@gmail.com>
2020-09-15 14:40:10 +0200

    JSON decoding

---
+++ outsideWeather/Models/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  outsideWeather
+//
+//  Created by Mateusz Uszyński on 15/09/2020.
+//  Copyright © 2020 Mateusz Uszyński. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- outsideWeather/Models/WeatherManager.swift
+++ outsideWeather/Models/WeatherManager.swift
@@ -20,24 +20,30 @@ struct WeatherManager {
         if let url = URL(string: urlString) { //create URl with optional string
             let session = URLSession(configuration: .default) //create URL Session
             
-
+            
             //give the session task
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             
             task.resume() //start the task
         }
     }
     
-    
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
 }
