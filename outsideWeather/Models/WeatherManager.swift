22ce8127e7dd28691029a0a43a39f34f32c2f8ab
uszynskimateusz <uszynskimateusz@gmail.com>
2020-09-15 14:20:09 +0200

    urlSession added

--- outsideWeather/Models/WeatherManager.swift
+++ outsideWeather/Models/WeatherManager.swift
@@ -9,9 +9,35 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "api.openweathermap.org/data/2.5/weather?appid=6c592962ba8ef814a41ba10d778da195&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=6c592962ba8ef814a41ba10d778da195&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) { //create URl with optional string
+            let session = URLSession(configuration: .default) //create URL Session
+            
+
+            //give the session task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            
+            task.resume() //start the task
+        }
+    }
+    
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
     }
 }
